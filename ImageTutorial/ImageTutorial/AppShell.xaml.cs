74cdcfbff42c07906dc81023c39ba364a77dcff1
User <User@WIN-C9HQIV4VA1U>
2021-12-07 22:24:07 +0300

    Add project files.

---
+++ ImageTutorial/ImageTutorial/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ImageTutorial.ViewModels;
+using ImageTutorial.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ImageTutorial
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
